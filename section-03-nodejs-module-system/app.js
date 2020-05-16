2b2b0b3a2907d137cc7b148c327db393a234357c
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-16 12:13:38 +0000

    CompleteNodeDevCourse: Challenge 01 of Section 3
    Section 03 : Node.js module system
    
    Challenge: Append a message to notes.txt
    Use appendFileSync to append to the file

--- section-03-nodejs-module-system/app.js
+++ section-03-nodejs-module-system/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My Name is Sandro.');
+// fs.writeFileSync('notes.txt', 'My Name is Sandro.');
+
+fs.appendFileSync('notes.txt', ' I am a Fullstack web developer and I am 31 year old');
