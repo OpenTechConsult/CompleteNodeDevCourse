dacfd1a2535a151ef611f264a9b74dcbf897550a
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-16 14:32:51 +0000

    CompleteNodeDevCourse: fs module - require a function created
    Section 03 : Node.js module system
    
    Challenge 2: Define and use a function in a new file
    
    1. create a new file file called notes.js
    2. create a getNotes function that returns the string "Your notes..."
    3. export getNotes function
    4. from app.js, load in and call the function printing message to console
    
    This challenge reinforce our ability to create a function in a separate file and
    require that function in our main file.

--- section-03-nodejs-module-system/app.js
+++ section-03-nodejs-module-system/app.js
@@ -1,5 +1,10 @@
-const add = require('./utils')
+const getNotes = require('./notes')
 
-const sum = add(4, -2);
+const notes = getNotes()
 
-console.log(sum);
+console.log(notes);
+
+
+// const add = require('./utils')
+// const sum = add(4, -2);
+// console.log(sum);
