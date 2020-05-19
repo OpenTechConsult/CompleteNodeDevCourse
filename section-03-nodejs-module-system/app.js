66535574da56ae8c5850cbedbcbd62d88e764f45
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-19 10:34:18 +0000

    CompleteNodeDevCourse: fs module - Challenge 3
        Section 03 : Node.js module system
    
        Challenge 3: Import and use NPM module called chalk
    
        1. install chalk npm module
        2. import the module in the app.js using the require syntax
        3. use the chalk module to print the text 'Success' in green

--- section-03-nodejs-module-system/app.js
+++ section-03-nodejs-module-system/app.js
@@ -1,11 +1,14 @@
-const validator = require('validator');
+// const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const notes = getNotes();
 
 console.log(notes);
-console.log(validator.isEmail('eagboka@opentechconsult.com'));
-console.log(validator.isURL('https:/mead.io'));
+// console.log(validator.isEmail('eagboka@opentechconsult.com'));
+// console.log(validator.isURL('https:/mead.io'));
+console.log(chalk.bold.green('Success!'));
+
 
 
 // const add = require('./utils')
