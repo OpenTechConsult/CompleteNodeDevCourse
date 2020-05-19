55f8d68d89a1ccc197d4c20ef8bd533de22aa678
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-19 10:04:25 +0000

    CompleteNodeDevCourse: Importing  NPM modules - import NPM module from NPM registry
    Section 03 : Node.js module system
    
    We learned how to use npm to install module from the npm registry
    We installed the validator module
    We then import it like we would with core module and we use it to validate an email and an URL string.

--- section-03-nodejs-module-system/app.js
+++ section-03-nodejs-module-system/app.js
@@ -1,8 +1,11 @@
-const getNotes = require('./notes')
+const validator = require('validator');
+const getNotes = require('./notes');
 
-const notes = getNotes()
+const notes = getNotes();
 
 console.log(notes);
+console.log(validator.isEmail('eagboka@opentechconsult.com'));
+console.log(validator.isURL('https:/mead.io'));
 
 
 // const add = require('./utils')
